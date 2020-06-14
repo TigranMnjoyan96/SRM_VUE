2dd5766515aa5aece9b19e4de7800d5bdee06eb2
TigranMnjoyan96 <mnjoyan1996@gmail.com>
2020-06-14 21:00:37 +0400

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="app-main-layout">
+   
+
+ <Navbar @hideSidebar="hideSidebar" />
+ <Sidebar :isOpen="isOpen" />
+
+    <main class="app-content" :class="{full: !isOpen}">
+        <div class="app-page">
+
+            <router-view />
+
+        </div>
+    </main>
+
+    <div class="fixed-action-btn">
+        <a class="btn-floating btn-large blue" href="#">
+            <i class="large material-icons">add</i>
+        </a>
+    </div>
+</div>
+</template>
+
+<script>
+import Navbar from './partials/Navbar'
+import Sidebar from './partials/Sidebar'
+
+
+export default {
+ name: 'MainLayout',
+ data() {
+  return {
+   isOpen: true
+  }
+ },
+ components: {
+  Navbar, Sidebar
+ },
+ methods: {
+  hideSidebar() {
+   this.isOpen = !this.isOpen
+  }
+ }
+}
+</script>
---
+++ src/layouts/partials/Sidebar.vue
@@ -0,0 +1,48 @@
+<template>
+<ul class="sidenav app-sidenav" :class="{open : isOpen}">
+    <router-link v-for="item in links" :key="item.link" tag="li" :to="item.link" exact active-class="active">
+        <a href="#" class="waves-effect waves-orange pointer">{{item.title}}</a>
+    </router-link>
+</ul>
+</template>
+
+<script>
+export default {
+    name: 'Sidebar',
+    props: ['isOpen'],
+    data() {
+        return {
+            isVisible: true,
+            links: [{
+                    link: '/',
+                    title: 'Счет'
+                },
+                {
+                    link: '/history',
+                    title: 'История'
+                },
+                {
+                    link: '/planning',
+                    title: 'Планирование'
+                },
+                {
+                    link: '/record',
+                    title: 'Новая запись'
+                },
+                {
+                    link: '/categories',
+                    title: 'Категории'
+                }
+            ]
+        }
+    },
+    // computed: {
+    //  si() {
+    //   console.log(this.isOpen);
+    //  }
+    // }
+    created() {
+     this.isOpen
+    }
+}
+</script>
